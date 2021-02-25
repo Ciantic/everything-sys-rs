a67503ae8dc59b78ddf9c24215c99ebf47d9aedc
Jari Pennanen <ciantic@oksidi.com>
2021-02-25 22:00:41 +0200

    ...

--- build.rs
+++ build.rs
@@ -2,43 +2,45 @@ use std::{env, path::Path};
 use std::{fs::OpenOptions, io::Write};
 
 fn main() {
-    let cargo_dir_str = &env::var("CARGO_MANIFEST_DIR").unwrap();
-    let cargo_dir = Path::new(cargo_dir_str);
-    let bindings_file = cargo_dir.join("src/lib.rs");
-    let bindings_header = cargo_dir.join("src/bindings.h").display().to_string();
+    if cfg!(not(memory_module_sys_docs_rs)) {
+        let cargo_dir_str = &env::var("CARGO_MANIFEST_DIR").unwrap();
+        let cargo_dir = Path::new(cargo_dir_str);
+        let bindings_file = cargo_dir.join("src/lib.rs");
+        let bindings_header = cargo_dir.join("src/bindings.h").display().to_string();
 
-    // Build everything
-    cc::Build::new()
-        .file(cargo_dir.join("Everything/src/Everything.c"))
-        .compile("Everything64");
+        // Build everything
+        cc::Build::new()
+            .file(cargo_dir.join("Everything/src/Everything.c"))
+            .compile("Everything64");
 
-    println!("cargo:rerun-if-changed={}", bindings_header);
+        println!("cargo:rerun-if-changed={}", bindings_header);
 
-    // Generate bindings.rs file
-    let output = bindgen::Builder::default()
-        .header(bindings_header)
-        .blacklist_type("*")
-        .whitelist_function("Everything.*")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .generate()
-        .expect("Unable to generate bindings")
-        .to_string();
+        // Generate bindings.rs file
+        let output = bindgen::Builder::default()
+            .header(bindings_header)
+            .blacklist_type("*")
+            .whitelist_function("Everything.*")
+            .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+            .generate()
+            .expect("Unable to generate bindings")
+            .to_string();
 
-    let mut output_contents = r#"
+        let mut output_contents = r#"
         use winapi::shared::windef::*;
         use winapi::shared::minwindef::*;
         use winapi::um::winnt::*;
         "#
-    .replace("        ", "");
+        .replace("        ", "");
 
-    output_contents.push_str(&output);
+        output_contents.push_str(&output);
 
-    OpenOptions::new()
-        .write(true)
-        .truncate(true)
-        .create(true)
-        .open(bindings_file)
-        .unwrap()
-        .write_all(output_contents.as_bytes())
-        .unwrap();
+        OpenOptions::new()
+            .write(true)
+            .truncate(true)
+            .create(true)
+            .open(bindings_file)
+            .unwrap()
+            .write_all(output_contents.as_bytes())
+            .unwrap();
+    }
 }
